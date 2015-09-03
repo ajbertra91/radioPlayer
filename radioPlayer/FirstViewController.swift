59710c3a316a9d8bf05a4adfe154ddf1cdca6647
Adam <adam@sportstechinc.com>
2015-09-03 19:14:24 -0400

    Initial Commit

---
+++ radioPlayer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  radioPlayer
+//
+//  Created by Adam Bertrand on 9/3/15.
+//  Copyright (c) 2015 Adam Bertrand. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
