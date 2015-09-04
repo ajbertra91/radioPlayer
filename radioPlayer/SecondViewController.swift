c2fb6e8c0653afc86085e08d4e7d90b70334c4d5
Adam <adam@sportstechinc.com>
2015-09-04 08:36:53 -0400

    initial commit

--- radioPlayer/SecondViewController.swift
+++ radioPlayer/SecondViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ radioPlayer/ShowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShowTableViewCell.swift
+//  radioPlayer
+//
+//  Created by Adam Bertrand on 9/3/15.
+//  Copyright (c) 2015 Adam Bertrand. All rights reserved.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
